4976c0dedb170ea5ec8bc9096d3a52fe670bd200
Suzanne Nichols <senichol@Suzannes-MacBook-Air.local>
2018-01-22 02:09:57 -0500

    added comments

--- makePlots.R
+++ makePlots.R
@@ -3,19 +3,19 @@ makePlots <- function() {
 	    powerCons = read.table("household_power_consumption.txt",skip=grep("^1/2/2007", readLines("household_power_consumption.txt")),nrows=2880, sep = ";")
 	    head(powerCons)
 
-	    pdf(file="hist.pdf")
+	    pdf(file="hist.pdf") ### make a histogram
 	    hist(powerCons$V3, col = "red", main = "Global active Power", xlab="Global active Power (Kilowats)")
 	    dev.off()
 
-	    dateAndTime <- paste(powerCons$V1, powerCons$V2)
+	    dateAndTime <- paste(powerCons$V1, powerCons$V2) ### convert the date and time to something useful
 	    DT <- strptime(dateAndTime, "%d/%m/%Y %H:%M:%S")
 
-	    pdf(file="Global.pdf")
+	    pdf(file="Global.pdf") ### plot global power
 	    plot(DT, powerCons$V3, ylab="Global active Power (Kilowats)", xlab= "Time", pch = '.')
 	    lines(DT, powerCons$V3)
 	    dev.off()
 
-	    pdf(file="EnergySub.pdf")
+	    pdf(file="EnergySub.pdf")  ### Plot power per sub meter
 	    plot(DT, powerCons$V7, ylab="Energy Sub metering", xlab= "Time", pch = '.')
 	    lines(DT, powerCons$V7)
 	    lines(DT, powerCons$V8, col = "red")
@@ -23,7 +23,7 @@ makePlots <- function() {
 	    legend("topright", col = c("black", "red","blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), pch = 20)
 	    dev.off()
 
-	    pdf(file="All.pdf")
+	    pdf(file="All.pdf") ### combine on one plot
 	    par(mfrow=c(2,2),mar=c(4,4,2,1))
             plot(DT, powerCons$V3, ylab="Global active Power (Kilowats)", xlab= "Time", pch = '.')
             lines(DT, powerCons$V3)
